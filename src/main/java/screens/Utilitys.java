ac901fd73a45ca0ed0efa906817e3831c146ed14
Amol Gavade <158724524+amolgavade0077@users.noreply.github.com>
2025-12-14 17:27:01 +0530

    Refactor Utilitys.java for code cleanup

--- src/main/java/screens/Utilitys.java
+++ src/main/java/screens/Utilitys.java
@@ -1,5 +1,4 @@
 package screens;
-
 import com.google.common.collect.ImmutableMap;
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.AppiumDriver;
@@ -11,10 +10,8 @@ import org.openqa.selenium.remote.RemoteWebElement;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.time.Duration;
 import java.util.logging.Logger;
-
 public class Utilitys {
 
 
@@ -23,36 +20,28 @@ public class Utilitys {
     public Utilitys(AppiumDriver driver) {
         this.driver = driver;
     }
-
     public void WaitToElementAppear(WebElement Element)
     {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
         wait.until(ExpectedConditions.visibilityOf(Element));
     }
-
     public void longPress(WebElement ele,int duration)
     {
         ((JavascriptExecutor) driver).executeScript("mobile:longClickGesture",ImmutableMap.of(
                 "elementId",((RemoteWebElement)ele).getId(),
                 "duration",duration));
     }
-
     public void scrollDown(String text) {
         driver.findElement(AppiumBy.androidUIAutomator(
                 "new UiScrollable(new UiSelector().scrollable(true))" +
                         ".scrollIntoView(new UiSelector().text(\"" + text + "\"))"));
-
     }
-
-
-
     public void SwipeGesture(WebElement ele, String direction) {
         ((JavascriptExecutor) driver).executeScript("mobile:swipeGesture", ImmutableMap.of("element",
                 ((RemoteWebElement) ele).getId(),
                 "direction", direction,
                 "percent", 0.20));
     }
-
     public void DragAndDrop(WebElement ele,int xCordi,int ycordi)
     {
         ((JavascriptExecutor) driver).executeScript("mobile: dragGesture", ImmutableMap.of(
@@ -60,5 +49,4 @@ public class Utilitys {
                 "endX", xCordi,
                 "endY", ycordi));
     }
-
 }
